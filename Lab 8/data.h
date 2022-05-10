8d7122d2522c35c149a2ef3ae7a9acf70e18bc9b
CoderMF <67932027+CoderMF@users.noreply.github.com>
2022-05-10 00:35:27 -0400

    Add files via upload

---
+++ Lab 8/data.h
@@ -0,0 +1,25 @@
+/*
+ -------------------------------------
+ File:    data.h
+ Project: R7
+ file description
+ -------------------------------------
+ Author:  Maham Farooq
+ ID:      200498180
+ Email:   faro8180@mylaurier.ca
+ Version  2021-06-22
+ -------------------------------------
+ */
+
+#ifndef DATA_H_
+#define DATA_H_
+
+#include "process.h"
+
+typedef Process Data;
+
+void print_data(Data *d);
+void destroy_data(Data **d);
+Data* copy_data(Data *d);
+
+#endif /* DATA_H_ */
